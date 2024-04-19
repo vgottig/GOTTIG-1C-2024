8ea434aa94d5bf8d117232aa49815c7b0984e94c
vgottig <valentinagottig@gmail.com>
2024-04-19 15:10:03 -0300

    Se sube el Ejercicio 2 del Proyecto 2

--- firmware/projects/Proyecto2_EJ2/main/Proyecto2_Ej2.c
+++ firmware/projects/Proyecto2_EJ2/main/Proyecto2_Ej2.c
@@ -3,7 +3,7 @@
  * @section genDesc General Description
  *
  * Se modifica la actividad del Ejercicio 1 de manera de utilizar interrupciones
- * para el control de las teclas y el control de tiempos (Timers). 
+ * para el control de las teclas y el control de tiempos (Timers).
  *
  * <a href="https://drive.google.com/...">Operation Example</a>
  *
@@ -39,20 +39,21 @@
 #include "lcditse0803.h"
 #include "hc_sr04.h"
 #include "delay_mcu.h"
+#include "timer_mcu.h"
 /*==================[macros and definitions]=================================*/
-#define CONFIG_BLINK_PERIOD 1000
-#define CONFIG_BLINK_PERIOD_2 50
+#define CONFIG_BLINK_PERIOD_LED_1_US 1000000
 
 /*==================[internal data definition]===============================*/
+TaskHandle_t led1_task_handle = NULL;
 
 /** @def tecla1
-*   @brief Activa y desactiva la medición
-*/ 
+ *  @brief Activa y desactiva la medición
+ */
 bool tecla1 = false;
 
 /** @def tecla2
-*   @brief Mantiene el resultado de la medición
-*/ 
+ *  @brief Mantiene el resultado de la medición
+ */
 bool tecla2 = false;
 
 /*==================[internal functions declaration]=========================*/
@@ -60,8 +61,8 @@ bool tecla2 = false;
 /** @fn void distancias_task(void *pvParameter)
  *  @brief Tarea que mide la distancia utilizando el sensor y los leds
  *  @param[in] void *pvParameter
- *  @return 
-*/
+ *  @return
+ */
 void distancias_task(void *pvParameter)
 {
 
@@ -69,14 +70,16 @@ void distancias_task(void *pvParameter)
 
 	while (1)
 	{
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // La tarea espera en este punto hasta recibir una notificación
+
 		if (tecla1)
 		{
 
 			distancia = HcSr04ReadDistanceInCentimeters();
 
-			if (!tecla2)
-			{								 // si no apreto la tecla2
-				LcdItsE0803Write(distancia); // pantalla LCD
+			if (!tecla2) // Si no apreto la tecla2
+			{								 
+				LcdItsE0803Write(distancia); // Pantalla LCD
 			}
 
 			if (distancia < 10)
@@ -110,47 +113,55 @@ void distancias_task(void *pvParameter)
 			LcdItsE0803Off();
 			LedsOffAll();
 		}
-
-		vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
 	}
 }
 
-/** @fn void switchTeclas_task(void *pvParameter)
- *  @brief Tarea que activa y detiene la medición utilizando la tecla 1 y mantiene un resultado utilizando la tecla 2
- *  @param[in] void *pvParameter
- *  @return 
-*/
-void switchTeclas_task(void *pvParameter)
+/**
+ * @brief Función para interrumpir con la tecla 1 
+ */
+void OnOff()
 {
-	uint8_t teclas;
-
-	while (1)
-	{
-		teclas = SwitchesRead();
-		switch (teclas)
-		{
-		case SWITCH_1:
-			// TEC1 para activar y detener la medición
-			tecla1 = !tecla1;
-			break;
-		case SWITCH_2:
-			// TEC2 para mantener el resultado (“HOLD”)
-			tecla2 = !tecla2;
-			break;
-		}
-
-		vTaskDelay(CONFIG_BLINK_PERIOD_2 / portTICK_PERIOD_MS);
-	}
+	tecla1 = !tecla1;
 }
+
+/**
+ * @brief Función para interrumpir con la tecla 2
+ */
+void mantenerResultado()
+{
+	tecla2 = !tecla2;
+}
+
+/**
+ * @brief Función invocada en la interrupción del timer 
+ */
+void FuncTimerB(void *param)
+{
+	vTaskNotifyGiveFromISR(led1_task_handle, pdFALSE); // Envía una notificación a la tarea asociada al LED_2
+}
+
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
-	LedsInit();
 	LcdItsE0803Init();
 	HcSr04Init(GPIO_3, GPIO_2);
 	SwitchesInit();
+	LedsInit();
 
-	xTaskCreate(&distancias_task, "distancias_task", 512, NULL, 5, NULL);
-	xTaskCreate(&switchTeclas_task, "switchTeclas_task", 512, NULL, 5, NULL);
+	// Llamada a todos los parámetros
+	timer_config_t my_timer = {
+		.timer = TIMER_B,
+		.period = 1000000,
+		.func_p = FuncTimerB,
+		.param_p = NULL};
+	TimerInit(&my_timer); // Inicialización del timer
+	TimerStart(my_timer.timer);
+
+	// Creación de tarea
+	xTaskCreate(&distancias_task, "distancias_task", 512, NULL, 5, &led1_task_handle);
+
+	// Interrupciones
+	SwitchActivInt(SWITCH_1, &OnOff, NULL);
+	SwitchActivInt(SWITCH_2, &mantenerResultado, NULL);
 }
 /*==================[end of file]============================================*/
